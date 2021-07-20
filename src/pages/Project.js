038c8db6a9e7d7feb5401197f623f4ed3cc24b9d
Thor Nguyen <viet@MacBook-Pro-cua-viet.local>
2021-07-20 13:37:21 +0700

    page 404 & page project

--- src/pages/Project.js
+++ src/pages/Project.js
@@ -8,30 +8,57 @@ const Project = () => {
           <h1>Project.</h1>
           <span className="lnr lnr-briefcase"></span>
         </div>
-        <div className="button-portfolio">
-          <span className="btn active-port">
-            all
-          </span>
-          <span className="btn" >
-            NHAT TIN LOGISTICS
-          </span>
-          <span className="btn" >
-            Freelancer
-          </span>
-        </div>
-        <div className="grid-portfolio">
-          <div className="product brand show">
-            <a href="https://watson-vcard.netlify.app/img/portfolio/img-1.jpg">
-              <figure className="effect-img">
-                <img
-                  src="https://watson-vcard.netlify.app/img/portfolio/img-1.jpg"
-                  alt=""
-                />
-                <figcaption>
-                  <h4>Book Design</h4>
-                  <p>Graphic</p>
-                </figcaption>
-              </figure>
+
+        <div class="row blogs-masonry">
+          <div class="project-item">
+            <a href="blog-dark.html" class="blog-item">
+              <div class="blog-image">
+                <img src="./image/img-1.jpeg" alt="" />
+              </div>
+              <div class="blog-content">
+                <span class="cat">Web Design</span>
+                <h4 class="blog-title">Best Way to Design</h4>
+                <div class="blog-date">August 15, 2018</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="project-item">
+            <a href="blog-dark.html" class="blog-item">
+              <div class="blog-image">
+                <img src="./image/img-1.jpeg" alt="" />
+              </div>
+              <div class="blog-content">
+                <span class="cat">Web Design</span>
+                <h4 class="blog-title">Best Way to Design</h4>
+                <div class="blog-date">August 15, 2018</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="project-item">
+            <a href="blog-dark.html" class="blog-item">
+              <div class="blog-image">
+                <img src="./image/img-1.jpeg" alt="" />
+              </div>
+              <div class="blog-content">
+                <span class="cat">Web Design</span>
+                <h4 class="blog-title">Best Way to Design</h4>
+                <div class="blog-date">August 15, 2018</div>
+              </div>
+            </a>
+          </div>
+
+          <div class="project-item">
+            <a href="blog-dark.html" class="blog-item">
+              <div class="blog-image">
+                <img src="./image/img-1.jpeg" alt="" />
+              </div>
+              <div class="blog-content">
+                <span class="cat">Web Design</span>
+                <h4 class="blog-title">Best Way to Design</h4>
+                <div class="blog-date">August 15, 2018</div>
+              </div>
             </a>
           </div>
         </div>
